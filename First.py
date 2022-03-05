5074c019b5786c6eabf5aafbda376adee21889f0
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-03-05 23:38:39 +0500

    Update File

---
+++ First.py
@@ -0,0 +1,21 @@
+# print("Hello")
+# firstname = "Tony "
+# lastname = " Stark "
+# fullname = firstname + lastname
+# print(fullname)
+
+# age = 47
+
+# genius = True
+
+# print(age)
+# print(genius)
+
+# name = input("What is your Age")
+# myname = int(name) + 2
+# print(myname)
+
+first = input("Enter Your First No")
+second = input("Enter second No")
+total = int(first) + int(second)
+print(" the sum of no is" + str(total))
